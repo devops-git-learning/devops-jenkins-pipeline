b4d72cfa0734d76965bdb712fd9d15a034086179
pkumarbe <mymail8500@gmail.com>
2020-10-17 17:26:45 +0530

    commit for build push run

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome to Devops jenkins pipeline learning"
+    return "Welcome to Devops jenkins pipeline "
 if __name__ == "__main__":
+
+    app.run(host='0.0.0.0', port=int('8001'), debug=True,threaded=True, use_reloader=True)
 
